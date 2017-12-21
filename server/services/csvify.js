b153225099608aec91a3a2e27e3eddb40ddc3659
Alex <alexander.weltman@colorado.edu>
2017-12-21 16:01:57 -0600

    secure downloading working

--- server/services/csvify.js
+++ server/services/csvify.js
@@ -1,25 +1,22 @@
 
-function CSVEscape(field) {
+const CSVEscape = (field) => {
   return '"' + String(field || "").replace(/\"/g, '""') + '"';
-}
+};
 
-var headers = [
+const headers = [
   'Plate Number',
   'State',
   'Added By',
   'Date'
 ].map(CSVEscape).join(',');
 
-function docToCSV(doc) {
+const docToCSV = (doc) => {
   return [
     doc.number,
     doc.state,
     doc.addedBy,
     doc.createdAt
   ].map(CSVEscape).join(',');
-}
-
-module.exports = {
-  headers: headers,
-  docToCSV: docToCSV
 };
+
+module.exports = { headers, docToCSV };
